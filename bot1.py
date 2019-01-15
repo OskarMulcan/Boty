ff9e1974085166d731fa8758513c93f864ce4a51
Marcel Olsen <marcelolsen26@gmail.com>
2019-01-15 19:44:27 +0100

    Somebody Toucha My Spaghet

---
+++ bot1.py
@@ -0,0 +1,9 @@
+import praw
+
+reddit = praw.Reddit('bot1')
+subreddit = reddit.subreddit("learnpython")
+
+for print("Title: ", submission.title)
+    print("Text: ", submission.selftext)
+    print("Score: ", submission.score)
+    print("---------------------------------\n")
