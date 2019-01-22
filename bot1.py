75feb16cd1207839fc81f702c497ccb1fef53628
Bartosz Wasilik <bartoszwasilik12@gmail.com>
2019-01-22 19:48:45 +0100

    dodano

--- bot1.py
+++ bot1.py
@@ -1,9 +1,10 @@
 import praw
 
 reddit = praw.Reddit('bot1')
-subreddit = reddit.subreddit("learnpython")
+subreddit = reddit.subreddit("BotPlayGroundSpam")
 
 for print("Title: ", submission.title)
     print("Text: ", submission.selftext)
     print("Score: ", submission.score)
     print("---------------------------------\n")
+
